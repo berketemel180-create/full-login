8923c6446923ee7fa6660c323bd99aa8d4a401ed
Berke Can Temel <berketemel180@gmail.com>
2025-08-12 17:01:26 +0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const app = express();
-
-app.use(express.json());
-
 const path = require('path');
+
+const app = express();
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/login', (req, res) => {
   const { musteriNo, sifre } = req.body;
-
   if (musteriNo === '8152887' && sifre === '123456') {
-    res.json({ message: 'Giriş başarılı' });
-  } else {
-    res.status(401).json({ message: 'Hatalı giriş' });
+    return res.json({ message: 'Giriş başarılı' });
   }
+  return res.status(401).json({ message: 'Hatalı giriş' });
 });
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT; 
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
